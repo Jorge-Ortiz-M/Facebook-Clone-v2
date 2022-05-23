52eb6b47d04475abefc20aa354a4e646c58846de
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-05-23 15:34:15 -0500

    Creating the needed actions in order to create posts, as well as the form and pages.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,21 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @post = Post.new
   end
 
-  def edit
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to root_path, notice: "#{@post.user.email}"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
