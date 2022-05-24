9e922210a0ae72c187c829b6a289f151ad843637
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-05-23 19:11:22 -0500

    Adding Google Oauth and Facebook Oauth.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable, :confirmable, :trackable
+    :recoverable, :rememberable, :validatable, :confirmable, :trackable, :omniauthable, :omniauth_providers => [:facebook, :google_oauth2, :github]
+
+    def self.from_omniauth(auth)
+      name_split = auth.info.name.split(" ")
+      user = User.where(email: auth.info.email).first
+      user ||= User.create!(provider: auth.provider, uid: auth.uid, email: auth.info.email, password: '123456')
+      user
+    end
   
   include RolesConcern
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :posts, only: [:new, :create, :edit, :update, :destroy]
-  devise_for :users
+
+  get 'omniauth_test', to: 'home#display_omniauth'
+  devise_for :users, controllers: {
+                                    omniauth_callbacks: 'users/omniauth_callbacks',
+                                    sessions: 'users/sessions'
+                                  }
+  devise_scope :user do
+    get 'sign_out' => 'devise/sessions#destroy'
+  end
+
   root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
