e60367502261520c90676638fcaaf3f9a216b948
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-05-23 19:30:25 -0500

    Adding Oauth with Github.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,6 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :github, Rails.application.credentials.github_oauth_client_id_prod, Rails.application.credentials.github_oauth_secret_key_prod, scope: 'user,public_repo'
   config.omniauth :facebook, Rails.application.credentials.facebook_oauth_app_id, Rails.application.credentials.facebook_oauth_secret_key
   config.omniauth :google_oauth2, Rails.application.credentials.google_oauth_client_id, Rails.application.credentials.google_oauth_client_secret, access_type: "online"
 
