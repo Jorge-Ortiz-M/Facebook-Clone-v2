7d879f861723e0072370678319e3269d54dbeefe
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-05-24 11:26:59 -0500

    Adding more tests to users and deleting default lines for posts test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+    let(:user) {
+        User.create(email: 'some@email.com', 
+                password: '123123', 
+                password_confirmation: '123123', 
+                approve: false,
+                role: 'member')
+    }
+
     it 'approve attribute must be false in new records' do
-        user = User.create(email: 'some@email.com', 
-            password: '123123', 
-            password_confirmation: '123123', 
-            approve: false) 
         expect(user.approve_must_be_false).to be false
     end
 
     it 'role attribute must be member in new records' do
-        user = User.create(email: 'some@email.com', 
-            password: '123123', 
-            password_confirmation: '123123', 
-            approve: false,
-            role: 'member') 
         expect(user.role_must_be_member).to eq('member')
+    end
+
+    it 'email shuold be presence' do 
+        expect(user.email.length).to be > 0
+    end
+
+    it 'password should have at least 6 characters' do
+        expect(user.password_length).to be >= 6
     end
 end
